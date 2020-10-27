c5a017cd3ec2a94eb00933fd10a53242d7da1832
Farhan Rafly Fahrezi <raflyfahr15@gmail.com>
2020-10-27 22:51:51 +0700

    recode App.js

--- App.js
+++ App.js
@@ -1,21 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { View, Text, SafeAreaView } from 'react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Assalamualaikum</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () => {
+    return (
+        <SafeAreaView>
+            <View>
+                <Text>Hello World</Text>
+            </View>
+        </SafeAreaView>
+    )
+}   
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
